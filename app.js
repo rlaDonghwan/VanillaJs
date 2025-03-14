45c7d97df4a9a2a813c9a06b1ecb85e32fb9729c
donghwan <kdhe9797@naver.com>
2025-03-14 16:48:34 +0900

    .

---
+++ app.js
@@ -0,0 +1,15 @@
+// const title = document.getElementById("title");
+// console.log(title);
+// console.dir(title);
+
+// title.innerText = "got u!";
+
+// title.log(className);
+// title.log(id);
+
+const title = document.querySelector("div.hello:first-child h1");
+
+console.log(title);
+
+titile.innerText = "Hello!";
+``;
